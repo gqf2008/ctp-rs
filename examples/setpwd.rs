14f590582368f09d470c1b45642d7f76538e05a5
高庆丰 <feng@hemayun.com>
2021-10-15 15:57:05 +0800

    no message

--- examples/setpwd.rs
+++ examples/setpwd.rs
@@ -64,13 +64,13 @@ fn main() -> Result<()> {
     tapi.login()?;
     std::thread::sleep(std::time::Duration::from_secs(1));
     tapi.update_passwd(opt.new_login_passwd.as_str())?;
-    std::thread::sleep(std::time::Duration::from_secs(1));
-    tapi.update_trade_account_passwd(
-        "ctp_dev_1",
-        "cny",
-        opt.trade_passwd.as_str(),
-        opt.new_trade_passwd.as_str(),
-    )?;
+    // std::thread::sleep(std::time::Duration::from_secs(1));
+    // tapi.update_trade_account_passwd(
+    //     "ctp_dev_1",
+    //     "cny",
+    //     opt.trade_passwd.as_str(),
+    //     opt.new_trade_passwd.as_str(),
+    // )?;
     tapi.wait()
 }
 
